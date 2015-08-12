130de510a1cd844c2705b6a3d55ea550a679f767
Sam Grimee <sgrimee@cisco.com>
2015-08-12 18:31:40 +0200

    fix name of web config file

--- graylog2-web/run_templates/run_graylog2_web.sh
+++ graylog2-web/run_templates/run_graylog2_web.sh
@@ -3,7 +3,9 @@
 GRAYLOG_SERVERS=${GRAYLOG_SERVERS:-graylog-server.skydns.local:12900}
 GRAYLOG_SERVER_SECRET=${GRAYLOG_SERVER_SECRET:-$(pwgen -s 96)}
 
-sed -i "s@GRAYLOG_SERVERS@$GRAYLOG_SERVERS@" /graylog2-web/conf/graylog2-web-interface.conf
-sed -i "s/GRAYLOG_SERVER_SECRET/$GRAYLOG_SERVER_SECRET/"  /graylog2-web/conf/graylog2-web-interface.conf
+sed -i "s@GRAYLOG_SERVERS@$GRAYLOG_SERVERS@" /graylog2-web/conf/graylog-web-interface.conf
+sed -i "s/GRAYLOG_SERVER_SECRET/$GRAYLOG_SERVER_SECRET/"  /graylog2-web/conf/graylog-web-interface.conf
+
+echo "GRAYLOG_SERVER_SECRET: $GRAYLOG_SERVER_SECRET"
 
 /graylog2-web/bin/graylog-web-interface
