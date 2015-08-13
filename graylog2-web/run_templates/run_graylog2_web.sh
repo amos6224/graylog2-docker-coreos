fba4213efd8a818477eb70b6b426d4122fa85b7c
Sam Grimee <sgrimee@cisco.com>
2015-08-13 15:47:17 +0200

    more configurable fields for UI

--- graylog2-web/run_templates/run_graylog2_web.sh
+++ graylog2-web/run_templates/run_graylog2_web.sh
@@ -2,8 +2,13 @@
 
 GRAYLOG_SERVERS=${GRAYLOG_SERVERS:-graylog2-server.skydns.local:12900}
 GRAYLOG_SERVER_SECRET=${GRAYLOG_SERVER_SECRET:-$(pwgen -s 96)}
+GRAYLOG_PREFIX=${GRAYLOG_PREFIX:-}
+GRAYLOG_TIMEZONE=${GRAYLOG_TIMEZONE:-Europe\/London}
 
-sed -i "s@GRAYLOG_SERVERS@$GRAYLOG_SERVERS@" /graylog2-web/conf/graylog-web-interface.conf
-sed -i "s/GRAYLOG_SERVER_SECRET/$GRAYLOG_SERVER_SECRET/"  /graylog2-web/conf/graylog-web-interface.conf
+CONFIG_FILE=/graylog2-web/conf/graylog-web-interface.conf
+sed -i "s@GRAYLOG_SERVERS@$GRAYLOG_SERVERS@" $CONFIG_FILE
+sed -i "s/GRAYLOG_SERVER_SECRET/$GRAYLOG_SERVER_SECRET/"  $CONFIG_FILE
+sed -i "s/GRAYLOG_PREFIX/$GRAYLOG_PREFIX/" $CONFIG_FILE
+sed -i "s/GRAYLOG_TIMEZONE/$GRAYLOG_TIMEZONE/" $CONFIG_FILE
 
 /graylog2-web/bin/graylog-web-interface
