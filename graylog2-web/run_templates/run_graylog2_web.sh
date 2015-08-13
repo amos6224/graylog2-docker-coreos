2e3044eb4e1b18a9de81aea7272958c4e9c70310
Sam Grimee <sgrimee@cisco.com>
2015-08-13 15:18:04 +0200

    change default server

--- graylog2-web/run_templates/run_graylog2_web.sh
+++ graylog2-web/run_templates/run_graylog2_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GRAYLOG_SERVERS=${GRAYLOG_SERVERS:-graylog-server.skydns.local:12900}
+GRAYLOG_SERVERS=${GRAYLOG_SERVERS:-graylog2-server.skydns.local:12900}
 GRAYLOG_SERVER_SECRET=${GRAYLOG_SERVER_SECRET:-$(pwgen -s 96)}
 
 sed -i "s@GRAYLOG_SERVERS@$GRAYLOG_SERVERS@" /graylog2-web/conf/graylog-web-interface.conf
