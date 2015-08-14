d2f2392ecf4907f4d56505019d4bbc522fa046a0
Sam Grimee <sgrimee@cisco.com>
2015-08-14 12:01:05 +0200

    better logs

--- graylog2-web/run_templates/run_graylog2_web.sh
+++ graylog2-web/run_templates/run_graylog2_web.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+ME=`basename "$0"`
+
 GRAYLOG_SERVERS=${GRAYLOG_SERVERS:-"http://graylog2-server.skydns.local:12900"}
 GRAYLOG_SERVER_SECRET=${GRAYLOG_SERVER_SECRET:-$(pwgen -s 96)}
 GRAYLOG_TIMEZONE=${GRAYLOG_TIMEZONE:-"Europe/London"}
+
+echo "$ME: Graylog servers: $GRAYLOG_SERVERS"
+echo "$ME: Graylog Timezone: $GRAYLOG_TIMEZONE"
 
 CONFIG_FILE=/graylog2-web/conf/graylog-web-interface.conf
 
@@ -12,9 +17,9 @@ sed -i "s@GRAYLOG_TIMEZONE@$GRAYLOG_TIMEZONE@" $CONFIG_FILE
 
 if [ -z ${GRAYLOG_PREFIX+x} ];
 	then 
-		echo "Leaving application.context to default";
+		echo "$ME: Leaving application.context to default";
 	else 
-		echo "Setting application context to $GRAYLOG_PREFIX"; 
+		echo "$ME: Setting application context to $GRAYLOG_PREFIX"; 
 		sed -i "s@#application.context=@application.context=$GRAYLOG_PREFIX@" $CONFIG_FILE
 fi
 
